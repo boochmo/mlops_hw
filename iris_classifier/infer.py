6663abf5e1fa88f360d2af46ce1a5acce008bc3a
boochmo <info@nanhekumar.com>
2023-10-20 23:39:00 +0300

    Installing Hydra

--- iris_classifier/infer.py
+++ iris_classifier/infer.py
@@ -1,26 +1,30 @@
+import hydra
 import joblib
 import pandas as pd
+from omegaconf import DictConfig
 from sklearn.metrics import classification_report
 
 
-def infer(model, test):
+@hydra.main(config_path="conf", config_name="config", version_base="1.3")
+def infer(cfg: DictConfig):
+    filename = f"{cfg['model']['name']}.sav"
+    model = joblib.load(filename)
+
+    PATH = cfg["data"]["path"]
+    test = pd.read_csv(f".{PATH}/test.csv")
+
     columns = ["Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width"]
     test_X = test[columns]
     test_y = test.Species
-    prediction = model.predict(test_X)
+    preds = model.predict(test_X)
 
-    print(classification_report(test_y, prediction))
+    print(classification_report(test_y, preds))
 
-    return prediction
+    preds = pd.DataFrame(preds)
+    pd.DataFrame(preds).to_csv(
+        cfg["infer"]["save_file"], sep=",", header=False, encoding="utf-8"
+    )
 
 
 if __name__ == "__main__":
-    filename = "irisclass.sav"
-    loaded_model = joblib.load(filename)
-    test = pd.read_csv("./data/test.csv")
-
-    preds = pd.DataFrame(infer(loaded_model, test))
-
-    pd.DataFrame(preds).to_csv(
-        "prediction.csv", sep=",", header=False, encoding="utf-8"
-    )
+    infer()
--- iris_classifier/train.py
+++ iris_classifier/train.py
@@ -1,22 +1,32 @@
+import hydra
 import joblib
 import pandas as pd
+from omegaconf import DictConfig
 from sklearn.linear_model import LogisticRegression
 
 
-def train(train):
+@hydra.main(config_path="conf", config_name="config", version_base="1.3")
+def train(cfg: DictConfig):
+    PATH = cfg["data"]["path"]
+    train = pd.read_csv(f".{PATH}/train.csv")
+
     columns = ["Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width"]
     train_X = train[columns]
     train_y = train.Species
 
-    model = LogisticRegression()
+    model = LogisticRegression(
+        penalty=cfg["model"]["penalty"],
+        tol=cfg["model"]["tol"],
+        C=cfg["model"]["C"],
+        fit_intercept=cfg["model"]["fit_intercept"],
+        max_iter=cfg["model"]["max_iter"],
+    )
+
     model.fit(train_X, train_y)
 
-    return model
+    filename = f"{cfg['model']['name']}.sav"
+    joblib.dump(model, filename)
 
 
 if __name__ == "__main__":
-    train_df = pd.read_csv("./data/train.csv")
-    model = train(train_df)
-
-    filename = "irisclass.sav"
-    joblib.dump(model, filename)
+    train()
